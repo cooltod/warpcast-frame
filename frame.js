26ae6f5c73cef07c7ec719c31e54f510ccdf5986
cooltod <78716147+cooltod@users.noreply.github.com>
2025-02-07 09:09:04 +0530

    Add files via upload

---
+++ frame.js
@@ -0,0 +1,12 @@
+https://coffee-oralle-35.tiiny.site
+export default function handler(req, res) {
+    res.json({
+        "frames": [
+            {
+                "image": "https://ibb.co/HL04tRwT",
+                "post_url": "https://coffee-oralle-35.tiiny.sitee",
+                "buttons": [{ "text": "Click Me", "action": "post" }]
+            }
+        ]
+    });
+}
